3ffca19039f627ead655fce81f8c179f2655c8ff
Piyush Mahajan <98900169+piyush-mahajan@users.noreply.github.com>
2022-11-24 19:13:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -106,11 +106,12 @@ function App() {
             )}
           </div>
         </nav>
-        <div className="flex w-screen min-h-screen flex-col items-center justify-center relative bg-primary pb-20">
+
+  <div className="w-screen min-h-screen flex-col items-center justify-center relative hidden lg:block">
           <Spline scene="https://prod.spline.design/xe1Uo-etdr8rz0t7/scene.splinecode" />
         </div>
-        <div className="relative w-full" id="home">
-          <div className="absolute bottom-10 w-full flex justify-center items-center">
+        <div className="relative w-full hidden lg:block " id="home">
+          <div className="absolute bottom-10 w-full flex justify-center items-center ">
             <div className="shadow-md p-4 flex items-center justify-center bg-zinc-900 rounded-3xl ">
               <p className="text-white">Press and drag to orbit</p>
             </div>
